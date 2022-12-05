846377bfb0320c54dda7439405a9762c302d8c3f
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2022-12-05 14:01:02 -0500

    styling - old tucson

--- index.js
+++ index.js
@@ -59,8 +59,9 @@ const renderImages = () => {
           assets/cactus-1900.avif?cb=${Date.now()} 1900w,
           assets/cactus-2100.avif?cb=${Date.now()} 2100w
         "
-        sizes="(max-width: 1300px) 100vw, (min-width: 1300px) 1300px"
+        sizes="(max-width: 1300px) 100vw, 1300px"
         type="image/avif"
+        height="747" width="1100"
       />
       <source
         srcset="
@@ -75,8 +76,9 @@ const renderImages = () => {
           assets/cactus-1900.webp?cb=${Date.now()} 1900w,
           assets/cactus-2100.webp?cb=${Date.now()} 2100w
         "
-        sizes="(max-width: 1300px) 100vw, (min-width: 1300px) 1300px"
+        sizes="(max-width: 1300px) 100vw, 1300px"
         type="image/webp"
+        height="747" width="1100"
       />
       <img
         srcset="
@@ -91,23 +93,24 @@ const renderImages = () => {
           assets/cactus-1900.jpg?cb=${Date.now()} 1900w,
           assets/cactus-2100.jpg?cb=${Date.now()} 2100w
         "
-        sizes="(max-width: 1300px) 100vw, (min-width: 1300px) 1300px"
+        sizes="(max-width: 1300px) 100vw, 1300px"
         src="assets/cactus-1100.jpg?cb=${Date.now()}"
         alt="Cactus image loaded responsively"
+        height="747" width="1100"
         
       />
     </picture>
-    <figcaption>RESPONSIVE</figcaption>
+    <figcaption>Responsive</figcaption>
   </figure>
 
   <figure>
     <img src="./assets/cactus-resized-and-compressed.jpg?cb=${Date.now()}" alt="Cactus image, resized and compressed" width="1500" height="1018" />
-    <figcaption>RESIZED &amp; COMPRESSED</figcaption>
+    <figcaption>Resized &amp; Compressed</figcaption>
   </figure>
 
   <figure>
     <img src="./assets/cactus-resized.jpg?cb=${Date.now()}" alt="Cactus image, resized" width="1500" height="1018" />
-    <figcaption>RESIZED</figcaption>
+    <figcaption>Resized</figcaption>
   </figure>
 
   <figure>
@@ -117,17 +120,17 @@ const renderImages = () => {
   
   <figure>
     <img src="./assets/cactus.webp?cb=${Date.now()}" alt="Cactus image in WEBP format" width="4678" height="3175" />
-    <figcaption>WEBP</figcaption>
+    <figcaption>WebP</figcaption>
   </figure>
 
   <figure>
     <img src="./assets/cactus-compressed.jpg?cb=${Date.now()}" alt="Cactus image, compressed" width="4678" height="3175" />
-    <figcaption>COMPRESSED</figcaption>
+    <figcaption>Compressed</figcaption>
   </figure>
 
   <figure>
     <img src="./assets/cactus.jpg?cb=${Date.now()}" alt="Cactus image, unoptimized" width="4678" height="3175" />
-    <figcaption>UNOPTIMIZED</figcaption>
+    <figcaption>Unoptimized</figcaption>
   </figure>`;
 
   const imageContainer = document.querySelector(".images");
@@ -153,16 +156,13 @@ window.addEventListener("load", () => {
       if (!img) return;
       const parent = img.closest("figure");
       const caption = parent.querySelector("figcaption").textContent;
+
       const duration = Math.round(resource.duration);
       durations.push({
         name: caption,
         duration,
         size: resource.encodedBodySize || null,
       });
-      const span = document.createElement("span");
-      span.classList.add("time");
-      span.textContent = formatDuration(duration);
-      parent.append(span);
     }
   });
 
@@ -178,16 +178,32 @@ window.addEventListener("load", () => {
 
     const name = document.createElement("div");
     name.classList.add("name");
-    name.textContent = `${el.name} (${formatDuration(el.duration)}${
-      el.size ? " - " + getReadableFileSizeString(el.size) : ""
-    })`;
+    name.innerHTML = `<div><svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+    <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+  </svg>${el.name}</div>
+  <div><svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+  <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+</svg>${formatDuration(el.duration)}</div>
+${
+  el.size
+    ? `<div><svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+      <path d="M12 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Zm-7.5.5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1ZM5 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM8 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"/>
+      <path d="M12 7a4 4 0 0 1-3.937 4c-.537.813-1.02 1.515-1.181 1.677a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588A4 4 0 0 1 8 3a4 4 0 0 1 4 4Zm-1 0a3 3 0 1 0-3.891 2.865c.667-.44 1.396-.91 1.955-1.268.224-.144.483.115.34.34l-.62.96A3.001 3.001 0 0 0 11 7Z"/>
+      <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2Zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4Z"/>
+      </svg>` +
+      getReadableFileSizeString(el.size) +
+      "</div>"
+    : ""
+}`;
 
     const duration = document.createElement("div");
     const amount = Number(el.duration / max).toFixed(2) * 100;
     duration.textContent = amount;
 
     const inner = document.createElement("div");
-    inner.style = `width: ${amount}%; height: 100%; background: #e70e60`;
+    inner.style = `width: ${amount}%; height: 100%; background: #7c7048`;
 
     div.append(inner);
     div.append(name);
